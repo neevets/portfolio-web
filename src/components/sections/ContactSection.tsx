ac88d157f5582bc9022859fb2be3cd1486ae6de1
Neevets <Neevetspaypal@gmail.com>
2026-04-17 19:55:03 +0200

    feat: adjust LinkedIn handle and increase Fiber icon size

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -40,7 +40,7 @@ const ContactSection = memo(() => {
     () => [
       {
         label: 'LinkedIn',
-        value: '/in/steeven-r-68921833a',
+        value: '/in/neevets',
         href: 'https://www.linkedin.com/in/steeven-r-68921833a',
         icon: ContactIcons.LinkedIn,
       },
